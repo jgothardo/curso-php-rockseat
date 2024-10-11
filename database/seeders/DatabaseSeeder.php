c5793f57e89af9055fd55eb1a667326a11734a87
Juliana Gothardo <juliana@gothardo.com>
2024-10-11 12:33:07 -0300

    proposals

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
+use App\Models\Proposal;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,9 +20,12 @@ class DatabaseSeeder extends Seeder
         User::factory()
         ->count(200)
         ->create();
-        
-        Project::factory()
-        ->count(10)
-        ->create();
+
+        User::query()->inRandomOrder()->limit(10)->get()
+        ->each(function (User $u) {
+           $project = Project::factory()->create(['created_by' => $u->id]);
+           Proposal::factory()->count(random_int(4,45))->create(['project_id' => $project->id]);
+
+        });
     }
 }
